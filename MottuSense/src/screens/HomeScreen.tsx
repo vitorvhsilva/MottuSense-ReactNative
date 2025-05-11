94ea7bbe3c0b82e92c07fe6f9b24224c72c07489
vitorvhsilva <dndragonbr@gmail.com>
2025-05-11 18:30:11 -0300

    Começando a criação da página inicial

--- MottuSense/src/screens/HomeScreen.tsx
+++ MottuSense/src/screens/HomeScreen.tsx
@@ -10,7 +10,23 @@ const HomeScreen = () => {
     <Container>
 
       <Content>
+        <HomeTitle>Seja bem vindo!</HomeTitle>
 
+        <AuthContent>
+          <SignUpButton>
+            <ButtonText>Cadastrar</ButtonText>
+          </SignUpButton>
+
+          <LoginTitle>
+            Já tem uma conta? 
+            <LoginLink 
+              //onPress={() => navigation.navigate('Login')}  
+            >
+              <LoginLinkText> Faça login.</LoginLinkText>
+            </LoginLink>
+
+          </LoginTitle>
+        </AuthContent>
       </Content>
     </Container>
   );
@@ -19,47 +35,59 @@ const HomeScreen = () => {
 const Container = styled.ScrollView`
   flex: 1;
   background-color: ${theme.colors.preto};
-`;
+  `;
 
 const Content = styled.View`
-  padding: 20px;
-`;
-
-/*
-const Input = styled.TextInput`
-  height: 40px;
-  border: 1px solid #ced4da;
-  border-radius: 5px;
-  margin-bottom: 10px;
-  padding: 10px;
-`;
-
-const AddButton = styled.TouchableOpacity`
-  background-color: #28a745;
-  padding: 10px;
-  border-radius: 5px;
-  margin-bottom: 20px;
+  padding: 40px;
+  display: flex;
+  justify-content: space-between;
+  height: 70vh;
   align-items: center;
+`;
+
+const AuthContent = styled.View`
+  align-items: center;
+  width: 100%;
+  height: fit-content;
+`;
+
+const SignUpButton = styled.TouchableOpacity`
+  background-color: ${theme.colors.verdeClaro1};
+  padding: 15px;
+  margin: 0px 40px;
+  width: 80%;
+  border-radius: 30px;
+  margin-bottom: 20px;
 `;
 
 const ButtonText = styled.Text`
-  color: white;
-  font-weight: bold;
+  color: ${theme.colors.branco};
+  font-weight: ${theme.typography.title.fontWeight};
+  font-size: ${theme.typography.title.fontSize};
+  text-align: center;
 `;
 
-const ListItem = styled.View`
-  background-color: white;
-  padding: 15px;
-  border-radius: 5px;
-  margin-bottom: 10px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: row;
+const HomeTitle = styled.Text`
+  color: ${theme.colors.branco};
+  font-weight: ${theme.typography.subtitle.fontWeight};
+  font-size: ${theme.typography.subtitle.fontSize};
 `;
 
-const ListItemText = styled.Text`
-  font-size: 16px;
+const LoginTitle = styled.Text`
+  color: ${theme.colors.branco};
+  font-weight: ${theme.typography.subtitle.fontWeight};
+  font-size: ${theme.typography.subtitle.fontSize};
 `;
-*/
+
+const LoginLinkText = styled.Text`
+  color: ${theme.colors.verdeClaro1};
+  font-weight: ${theme.typography.title.fontWeight};
+  font-size: ${theme.typography.subtitle.fontSize};
+  border-bottom-width: 1px;
+  border-bottom-color: ${theme.colors.verdeClaro1};
+`;
+
+const LoginLink = styled.TouchableOpacity`
+`;
+
 export default HomeScreen;
